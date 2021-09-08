1c7b720b579e541de497020f87654e31dfc08e21
Ahmed Ramy <ahmedramy.e404@gmail.com>
2021-09-08 20:08:36 +0200

    feat: Added addPost page

--- src/Components/Posts/Post/Post.js
+++ src/Components/Posts/Post/Post.js
@@ -78,6 +78,96 @@ const Post = ({ post }) => {
                     return <Comment comment={comment} />;
                   })
                 : null}
+              <div className="addCommentbtn">
+                <div>
+                  <div
+                    className="btn-group"
+                    role="group"
+                    aria-label="Basic example"
+                  >
+                    <button
+                      type="button"
+                      className="btn btn-primary"
+                      data-bs-toggle="modal"
+                      data-bs-target="#exampleModal"
+                      data-bs-whatever="@mdo"
+                    >
+                      Add Movie
+                    </button>
+                  </div>
+
+                  <div
+                    className="modal fade"
+                    id="exampleModal"
+                    tabIndex="-1"
+                    aria-labelledby="exampleModalLabel"
+                    aria-hidden="true"
+                  >
+                    <div className="modal-dialog">
+                      <div className="modal-content">
+                        <div className="modal-header">
+                          <h5 className="modal-title" id="exampleModalLabel">
+                            New Movie
+                          </h5>
+                          <button
+                            type="button"
+                            className="btn-close"
+                            data-bs-dismiss="modal"
+                            aria-label="Close"
+                          ></button>
+                        </div>
+                        <div className="modal-body">
+                          <form>
+                            <div className="mb-3">
+                              <label
+                                htmlFor="user-email"
+                                className="col-form-label"
+                              >
+                                email
+                              </label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                id="user-email"
+                                required
+                              />
+                            </div>
+                            <div className="mb-3">
+                              <label
+                                htmlFor="comment-body"
+                                className="col-form-label"
+                              >
+                                comment
+                              </label>
+                              <input
+                                type="text"
+                                className="form-control"
+                                id="comment-body"
+                              />
+                            </div>
+                          </form>
+                        </div>
+                        <div className="modal-footer">
+                          <button
+                            type="button"
+                            className="btn btn-secondary"
+                            data-bs-dismiss="modal"
+                          >
+                            Close
+                          </button>
+                          <button
+                            type="button"
+                            data-bs-dismiss="modal"
+                            className="btn btn-primary"
+                          >
+                            Add Comment
+                          </button>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
         </div>
