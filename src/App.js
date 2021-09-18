1c15d1e8345ac1a8097e357c59bc8a7cf955c14c
Ahmed Ramy <64094908+aahmedramy1@users.noreply.github.com>
2021-09-18 16:12:01 +0200

    fix: :bug: Fixed api bug
    changed useEffect position and fixed bug

--- src/App.js
+++ src/App.js
@@ -1,10 +1,78 @@
 import "./App.css";
+import React, { useEffect } from "react";
 import Header from "./Components/Header/Header";
 import PostList from "./Components/Posts/PostList/PostList";
 import { Route, Switch } from "react-router-dom";
 import AddPost from "./Components/AddPost/AddPost";
+import axios from "axios";
+import { useDispatch } from "react-redux";
+import { addPost } from "./Components/Slices/rootSlice";
 import PostDetail from "./Components/PostDetail/PostDetail";
+
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const getData = () => {
+      axios
+        .get("https://jsonplaceholder.typicode.com/posts")
+        .then((res) => {
+          const allPosts = res.data;
+          for (let i = 0; i < allPosts.length; i++) {
+            let currentPost = allPosts[i];
+            axios
+              .get(`https://jsonplaceholder.typicode.com/photos?id=${i + 1}`)
+              .then((res) => {
+                let photo = res.data[0];
+                axios
+                  .get(
+                    `https://jsonplaceholder.typicode.com/comments?postId=${
+                      i + 1
+                    }`
+                  )
+                  .then((res) => {
+                    let comments = res.data;
+                    axios
+                      .get(
+                        `https://jsonplaceholder.typicode.com/users?id=${currentPost.userId}`
+                      )
+                      .then((res) => {
+                        const user = res.data[0];
+                        const final = {
+                          userId: currentPost.userId,
+                          name: user.name,
+                          username: user.username,
+                          email: user.email,
+                          postId: currentPost.id,
+                          url: photo.url,
+                          thumbnailUrl: photo.thumbnailUrl,
+                          title: currentPost.title,
+                          body: currentPost.body,
+                          comments: comments,
+                          showComments: false,
+                        };
+
+                        dispatch(addPost(final));
+                      })
+                      .catch((e) => {
+                        console.log(`error getting user ${e}`);
+                      });
+                  })
+                  .catch((e) => {
+                    console.log(`error getting comments ${e}`);
+                  });
+              })
+              .catch((e) => {
+                console.log(`error getting photo ${e}`);
+              });
+          }
+        })
+        .catch((e) => {
+          console.log(`error getting posts ${e}`);
+        });
+    };
+    getData();
+  }, [dispatch]);
+
   return (
     <div>
       <Header />
--- src/Components/Posts/PostList/PostList.js
+++ src/Components/Posts/PostList/PostList.js
@@ -1,8 +1,7 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Post from "../Post/Post";
-import { morePosts, addPost } from "../../Slices/rootSlice";
-import axios from "axios";
+import { morePosts } from "../../Slices/rootSlice";
 import "./PostList.css";
 
 const PostList = () => {
@@ -11,67 +10,6 @@ const PostList = () => {
   const searchValue = useSelector((state) => state.searchValue);
   const dispatch = useDispatch();
   console.log(searchValue);
-  useEffect(() => {
-    const getData = () => {
-      axios
-        .get("https://jsonplaceholder.typicode.com/posts")
-        .then((res) => {
-          const allPosts = res.data;
-          for (let i = 0; i < allPosts.length; i++) {
-            let currentPost = allPosts[i];
-            axios
-              .get(`https://jsonplaceholder.typicode.com/photos?id=${i + 1}`)
-              .then((res) => {
-                let photo = res.data[0];
-                axios
-                  .get(
-                    `https://jsonplaceholder.typicode.com/comments?postId=${
-                      i + 1
-                    }`
-                  )
-                  .then((res) => {
-                    let comments = res.data;
-                    axios
-                      .get(
-                        `https://jsonplaceholder.typicode.com/users?id=${currentPost.userId}`
-                      )
-                      .then((res) => {
-                        const user = res.data[0];
-                        const final = {
-                          userId: currentPost.userId,
-                          name: user.name,
-                          username: user.username,
-                          email: user.email,
-                          postId: currentPost.id,
-                          url: photo.url,
-                          thumbnailUrl: photo.thumbnailUrl,
-                          title: currentPost.title,
-                          body: currentPost.body,
-                          comments: comments,
-                          showComments: false,
-                        };
-
-                        dispatch(addPost(final));
-                      })
-                      .catch((e) => {
-                        console.log(`error getting user ${e}`);
-                      });
-                  })
-                  .catch((e) => {
-                    console.log(`error getting comments ${e}`);
-                  });
-              })
-              .catch((e) => {
-                console.log(`error getting photo ${e}`);
-              });
-          }
-        })
-        .catch((e) => {
-          console.log(`error getting posts ${e}`);
-        });
-    };
-    getData();
-  }, [dispatch]);
 
   return (
     <div>
